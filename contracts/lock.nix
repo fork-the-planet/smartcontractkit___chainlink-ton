37c85f3ebb36957093573a470c479805a8ee4427
Patricio <contact@patricios.space>
2026-04-17 10:39:12 -0300

    [NONEVM-4523] [Onchain] Implement migration to change the token price staleness threshold in the Fee Quoter (#701)
    * feat: migrate function
    
    * fix: upgradeable test
    
    * test: wip state change
    
    * fix: feeQuoter data loader
    
    * feat: load code from releases
    
    * fix: compare storage with tonDeepEqual
    
    * fix: bump nix hash
    
    * wip: test overwrite murmur3
    
    * rm root @multiformats/murmur3
    
    * fix: pin tolk-js version
    
    * ref: use nix build instead and refactor local and release caches to resuse common behaviour
    
    * ref: group value functions into object
    
    * fix: bump contract version
    
    * fix: remove packages that are no longer required
    
    * fix: bump nix hash
    
    * fix: apply suggestions
    
    * fix: update target value
    
    * fix: command formatting in buildReleasePath
    
    * fix: raise setup timeout
    
    * fix: update tokenPriceStalenessThreshold
    
    * Fetch CCIP 1.6.0 contract package from GH release (tag)
    
    * ref: rename previous version
    
    * ref: rework contract loading to use directory from nix environment
    
    * doc: update documentation
    
    * fix: update expected value in new version
    
    ---------
    
    Co-authored-by: Kristijan <kristijan.rebernisak@gmail.com>

--- contracts/lock.nix
+++ contracts/lock.nix
@@ -1,6 +1,6 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `lock-nix-tidy` will only replace actual hashes.
 {pkgs}: {
-  contracts = "sha256-TuW9wXjtxq3y0UajUNJXJBNy96m3DzSPDLzI8XATg8o=";
+  contracts = "sha256-c7Qty4uMeJASTle0yEgvTCle3KpMaZrYYyB9SO10kXo=";
   contracts-jetton = "sha256-EZtvTf19MjSKTWNir6pcP9XHwUIpE4ILSlhS+cQD/7w=";
 }
